2b54069191d50598bd1def77ce96318409bbb112
artem2004maj <artemichuprov1994@gmail.com>
2026-03-16 13:02:19 +0300

    cltkfk

--- salon-elena/routes/web.php
+++ salon-elena/routes/web.php
@@ -5,6 +5,7 @@ use Illuminate\Support\Facades\Auth;
 use App\Http\Controllers\Auth\RegisteredUserController;
 use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Request;
 use Inertia\Inertia;
 
 // Главная страница
